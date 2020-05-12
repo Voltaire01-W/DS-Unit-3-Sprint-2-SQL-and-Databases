70e5261d53fb9c4173a2df179bb671c6dbacad5d
Voltaire01-W <jamesisanerd56@gmail.com>
2020-05-12 18:35:03 -0500

    Assignment 2

--- module2-sql-for-analysis/insert_titanic.py
+++ module2-sql-for-analysis/insert_titanic.py
@@ -1,14 +1,18 @@
 import os
 from dotenv import load_dotenv
-import psycopg2
-import sqlite3
 import pandas as pd
+import psycopg2
+from psycopg2.extras import execute_values
+import json
+import numpy as np
 
-df = pd.read_csv('titanic.csv')
-df['Name'] = df['Name'].str.replace("'", "", regex=True)
-df.head()
-
+psycopg2.extensions.register_adapter(np.int64, psycopg2._psycopg.AsIs)
 load_dotenv()
+csv = os.path.join(os.path.dirname(__file__), "..", "module2-sql-for-analysis", "titanic.csv")
+df = pd.read_csv(csv)
+df.index += 1
+x = list(df.to_records(index=True))
+
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PW")
@@ -16,30 +20,25 @@ DB_HOST= os.getenv("DB_HOST")
 
 conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
 curs = conn.cursor()
-curs.execute('DROP TABLE titanic_table')
+
+query = """
+CREATE TABLE IF NOT EXISTS passengers (
+    id SERIAL PRIMARY KEY,
+    survived integer,
+    pclass integer,
+    name varchar NOT NULL,
+    sex varchar NOT NULL,
+    age float,
+    sib_spouse_count integer,
+    parent_child_count integer,
+    fare float
+);
+"""
+curs.execute(query)
+
+tuples = x
+insert_query = "INSERT INTO passengers (id, survived, pclass, name, sex, age, sib_spouse_count, parent_child_count, fare) VALUES %s"
+execute_values(curs, insert_query, tuples)
 conn.commit()
-
-curs.execute('''CREATE TABLE titanic_table (
-                   id SERIAL PRIMARY KEY,
-                   survived INT,
-                   pclass INT,
-                   name VARCHAR(100),
-                   sex VARCHAR(10),
-                   age FLOAT,
-                   sibs_spouse INT,
-                   par_child INT,
-                   fare FLOAT);
-                ''')
-conn.commit()
- 
-sl_conn = sqlite3.connect('titantic_temp.sqlite3')
-sl_curs = sl_conn.cursor()
-
-df.to_sql('titanic_temp', sl_conn)
-
-passengers = sl_curs.execute('SELECT * FROM titanic_temp;').fetchall()
-
-for passenger in passengers:
-    curs.execute("INSERT INTO titanic_table VALUES" + str(passenger) + ";")
-
-conn.commit()
+curs.close()
+conn.close()
